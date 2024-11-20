99c3ace02169cd2da9f2943de5ea0e9a6d90ae49
naruko_hstk <34113239+naruko-hstk@users.noreply.github.com>
2024-11-20 18:50:57 +0800

    feat: update event

--- src/assets/events.js
+++ src/assets/events.js
@@ -1,17 +1,17 @@
-export default [
-  // TODO: 請複製下方的範例並修改對應資料
-  /*
-  {
-    title: "", // TODO: 活動名稱
-    repo: "", // TODO: 活動repo(https://gitlab.com/之後的部分) 不必加上.git或是/結尾
-    inboxLabel: `Inbox`, // TODO: 建立Inbox標籤
-    groupLabelPrefix: ``, // TODO: 非付費版請留空 付費版請填入前綴(e.g. 'Team::' 或 'Group::' 總之就是名稱後面要加上雙冒號)
-    groupLabelPostfix: ``, // TODO: 非必要 僅在有通用後綴(e.g. XX組 組是通用後綴)時填入
-    groupList: {
-      '': [''], // TODO: 把前面改成組名 後面改成組長id 例如: 'team_name': ['naruko_hstk']
-    },
+// TODO: 請複製下方的範例並修改對應資料
+/*
+{
+  title: "", // TODO: 活動名稱
+  repo: "", // TODO: 活動repo(https://gitlab.com/之後的部分) 不必加上.git或是/結尾
+  inboxLabel: `Inbox`, // TODO: 建立Inbox標籤
+  groupLabelPrefix: ``, // TODO: 非付費版請留空 付費版請填入前綴(e.g. 'Team::' 或 'Group::' 總之就是名稱後面要加上雙冒號)
+  groupLabelPostfix: ``, // TODO: 非必要 僅在有通用後綴(e.g. XX組 組是通用後綴)時填入
+  groupList: {
+    '': [''], // TODO: 把前面改成組名 後面改成組長id 例如: 'team_name': ['naruko_hstk']
   },
-  */
+},
+*/
+export default [
   {
     title: "なるこStudio",
     repo: "naruko.studio/boarder",
